ee999e7f54feb32e627c50593c26d31a5a0d6ffe
pingdrew <pingdrew0@gmail.com>
2023-08-04 16:47:27 -0500

    fixed file structure, added inquirer

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 // Import and require mysql2
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const db = mysql.createConnection(
+  // Connect to database
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'tracker_db'
+  },
+);
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,19 +18,6 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// Connect to database
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    // MySQL username,
-    user: 'root',
-    // MySQL password
-    password: 'root',
-    database: 'movieReviews_db'
-  },
-  console.log(`Connected to the movieReviews_db database.`)
-);
 
 var createdMovie = 'createdmovie'
 // Hardcoded query: ADD TO course_names WHERE id = 3;
@@ -42,9 +39,11 @@ db.query(`INSERT INTO reviews (movie_id, review) VALUES (?, ?)`, createdReview, 
 });
 
 // Query database
-db.query(`SELECT movies.movie_name AS movie, reviews.review AS review
-FROM reviews
-JOIN movies ON movies.id = reviews.movie_id;`, function (err, results) {
+db.query(`SELECT employee.first_name AS first_name, employee.last_name AS last_name, role.title AS title, role.salary AS salary, department.name AS department
+FROM role
+JOIN department ON role.department = department.id
+JOIN employee ON role.id = employee.id;`
+,function (err, results) {
   console.log(results);
 });
 
@@ -56,3 +55,29 @@ app.use((req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// GIVEN a command-line application that accepts user input
+
+// WHEN I start the application
+// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+
+// WHEN I choose to view all departments
+// THEN I am presented with a formatted table showing department names and department ids
+
+// WHEN I choose to view all roles
+// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+
+// WHEN I choose to view all employees
+// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+
+// WHEN I choose to add a department
+// THEN I am prompted to enter the name of the department and that department is added to the database
+
+// WHEN I choose to add a role
+// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+
+// WHEN I choose to add an employee
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+
+// WHEN I choose to update an employee role
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
